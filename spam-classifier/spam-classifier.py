4ccf8cc70ab5f765843de99e6b9e43527a06782d
Adeet Patel <adeet.patel@gmail.com>
2020-02-17 19:25:07 -0500

    seaborn countplot

--- spam-classifier/spam-classifier.py
+++ spam-classifier/spam-classifier.py
@@ -9,3 +9,9 @@ df.rename(columns = {"v1": "label",
                      "v2": "message"}, inplace = True) # this does the same thing as df = df.rename(...)
 
 df.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"], axis = 1, inplace = True)
+
+# See how many spam vs. ham messages there are in the dataset
+sns.countplot(df["label"])
+plt.title("Spam vs. Ham Messages")
+plt.xlabel("Label")
+plt.ylabel("Count")
